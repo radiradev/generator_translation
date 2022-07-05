afa72eb23bc138d33f97bfe1533eed9fa433b960
radiradev <radi.radev.uk@gmail.com>
2022-07-05 17:15:18 +0200

    updated shuffling

--- src/global_shuffle.py
+++ src/global_shuffle.py
@@ -1,27 +1,18 @@
 import dask.dataframe as dd
 import numpy as np
 import pandas as pd
-import dask.array as da
-from sklearn.utils import shuffle
+import glob
+import ray
 
-def set_random_indices(df):
-    indices = np.arange(len(df))
-    chunks = tuple(df.map_partitions(len).compute())
-    size = sum(chunks)
-    rand_indices = da.from_array(np.random.choice(indices, size=len(indices), replace=False), chunks=chunks)
-    df['rand_index'] = rand_indices
-    return df
 
-def shuffle_data(generator_name, root_dir='/eos/home-r/rradev/generator_reweigthing/from_h5/'):
-    mask = f'*train_data*{generator_name}*.csv'
-    df = dd.read_csv(root_dir + mask)
-    df = set_random_indices(df)
-    df = df.drop(columns=['Unnamed: 0', 'rand_index'])
-    return df
+root_dir = '/mnt/rradev/parquet_data/'
+print('reading paths')
+paths = [
+    root_dir + 'GENIEv2_train.parquet',
+    root_dir + 'NEUT_train.parquet']
+ray_df = ray.data.read_parquet(paths)
+ray_df = ray_df.repartition(num_blocks=100, shuffle=True)
 
-generator_names = ['GENIE, NEUT']
-root_dir = '/eos/home-r/rradev/generator_reweigthing/from_h5/'
-
-for generator_name in generator_names:
-    df = shuffle(generator_name)
-    df.to_csv(root_dir + '/global_shuffle/' + 'train_'+ generator_name + '_*.csv') 
+# Return a dask dataframe
+dask_df =  ray_df.to_dask()
+dask_df.to_csv(root_dir + '/global_shuffle/' + '*_train_' + '.csv')
