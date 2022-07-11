fc0e414101801caab3aeb3ec32bf3acbceb803ab
radiradev <radi.radev.uk@gmail.com>
2022-07-11 17:27:46 +0200

    ratio plots

--- utils/plotting.py
+++ utils/plotting.py
@@ -16,7 +16,8 @@ rc('legend', fontsize=15)
 rc('figure', figsize=(15, 10))
 
 
-def plot_distribution(nominal, target, weights, n_bins=50, range=None):
+
+def plot_distribution(nominal, target, weights, n_bins=50, range=None, errorbars=False):
     if range is None:
         min_val = 0
         max_val = np.min([
@@ -24,8 +25,29 @@ def plot_distribution(nominal, target, weights, n_bins=50, range=None):
             np.max(target)])
         range = (min_val, max_val)
 
-    plt.hist(target, label='Target', bins=n_bins, **plot_style_0, range=range)
-    plt.hist(nominal, label='Nominal', bins=n_bins,  **plot_style_1, range=range)
-    plt.hist(nominal, weights=weights, bins=n_bins, label='Nominal to Target', **plot_style_1, range=range)
-    plt.legend()
+    fig, axs = plt.subplots(2, 1)
 
+    counts_ref, bins_ref, _ = axs[0].hist(target, label='Target', bins=n_bins, **plot_style_0, range=range)
+    _, _, _ = axs[0].hist(nominal, label='Nominal', bins=n_bins,  **plot_style_1, range=range)
+    counts, _, _ = axs[0].hist(nominal, weights=weights, bins=n_bins, label='Nominal to Target', **plot_style_1, range=range)
+
+    x_values, y_values, y_errors = ratios(bins_ref, counts_ref, counts)
+    axs[1].plot(x_values, np.ones_like(counts_ref), '--')
+    if errorbars:
+        axs[1].errorbar(x=x_values, y=y_values, yerr=y_errors, fmt='o')
+        axs[1].set_ylim(0.5, 1.5)
+    else:
+        axs[1].plot(x_values, y_values, 'o')
+        axs[1].set_ylim(0.5, 1.5)
+
+def ratios(bins_ref, counts_ref, counts):
+    x_values = [0.5*(bins_ref[i+1]+bins_ref[i]) for i in range(len(counts_ref))]
+    
+    y_values = (counts + 1e-9)/(counts_ref + 1e-9)
+    y_errors = np.sqrt(counts)/(counts + 1e-9) \
+             + np.sqrt(counts_ref)/(counts_ref + 1e-9)
+    y_errors *= y_values
+    
+    return x_values, y_values, y_errors
+
+    
