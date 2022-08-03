7061a4344c378ed42b9fe90396ed0c05cd158311
radiradev <radi.radev.uk@gmail.com>
2022-08-03 17:34:14 +0200

    training with numpy arrays

--- src/root_dataloader.py
+++ src/root_dataloader.py
@@ -1,3 +1,4 @@
+from email import generator
 import itertools
 from matplotlib.style import library
 import uproot
@@ -5,6 +6,60 @@ import awkward as ak
 import numpy as np
 from glob import glob
 from torch.utils.data import Dataset
+
+
+class NumPyDataset(Dataset):
+    """
+    Loads converted ROOT files (numpy) from two generators into memory
+    """
+
+    def __init__(self,
+                 root_dir,
+                 generator_a='GENIEv2',
+                 generator_b='NUWRO',
+                 number_of_files=1,
+                 shuffle=True):
+        super().__init__()
+        self.root_dir = root_dir
+        self.generator_a = generator_a
+        self.generator__b = generator_b
+        self.shuffle = shuffle
+        self.number_of_files = number_of_files
+        self.dataset_a = self.load_generator(generator_a)
+        self.dataset_b = self.load_generator(generator_b)
+        self.data = self.load_data(self.dataset_a, self.dataset_b)
+
+    def __getitem__(self, index):
+        features, label = self.data[index][:-1], self.data[index][-1]
+        # Replace nan features
+        if np.isnan(features[9]):
+            features[9] = np.random.random() * 10
+
+        return {'features': features, 'label': label}
+
+    def __len__(self):
+        return len(self.data)
+
+    def draw_index(self, max_index=50):                    
+        return str(np.random.randint(low=0,high=max_index))
+
+    def load_generator(self, generator_name):
+        data = np.load(self.root_dir + generator_name + self.draw_index() + '.npy')
+        labels = self.create_labels(data, generator_name)
+        return np.hstack([data, labels])
+
+    def load_data(self, dataset_a, dataset_b):
+        data = np.vstack([dataset_a, dataset_b])
+        if self.shuffle:
+            np.random.shuffle(data)
+        return data
+
+    def create_labels(self, data, filename):
+        if self.generator_a in filename:
+            labels = np.zeros(len(data))
+        else:
+            labels = np.ones(len(data))
+        return np.expand_dims(labels, axis=1)
 
 
 class ROOTDataset(Dataset):
@@ -20,12 +75,20 @@ class ROOTDataset(Dataset):
                  shuffle=True,
                  preload_data=True,
                  sqrt_counts=False,
-                 test_data=False):
-        super(ROOTDataset, self).__init__()
+                 test_data=False,
+                 gibuu_dir='/afs/cern.ch/work/r/rradev/gibuu_files/',
+                 number_of_indices=1):
+        super().__init__()
         self.data_dir = data_dir
+        self.generator_a = generator_a
+        self.generator_b = generator_b
+        self.weights_sum_a = 0
+        self.weights_sum_b = 0
+        self.gibuu_dir = gibuu_dir
         self.shuffle = shuffle
         self.sqrt_counts = sqrt_counts
         self.test_data = test_data
+        self.number_of_indices = number_of_indices
         if preload_data:
             self.dataset_a = self.load_generator(generator_a)
             self.dataset_b = self.load_generator(generator_b)
@@ -33,7 +96,8 @@ class ROOTDataset(Dataset):
 
     def __getitem__(self, index):
 
-        features, label, weights = self.data[index][:-2], self.data[index][-2], self.data[index][-1]
+        features, label, weights = self.data[index][:-2], self.data[index][
+            -2], self.data[index][-1]
 
         # Replace nan features
         if np.isnan(features[9]):
@@ -54,22 +118,22 @@ class ROOTDataset(Dataset):
         """
         Loads generator file
         """
-        if generator_name == 'GiBUU':
-            wildcard_pattern = self.get_wildcard_pattern(generator_name)
-            filenames = [
-                glob(self.data_dir + pattern) for pattern in wildcard_pattern
-            ]
-            # Flatten 2D array
-            filenames = list(itertools.chain(*filenames))
-        else:
-            wildcard_pattern = self.get_wildcard_pattern(generator_name)
-            filenames = glob(self.data_dir + wildcard_pattern)
-            # The dataset has to include all pdg types (should not be a big issue for GiBUU)
-            len_pdg_types = 4
+        data_dir = self.data_dir
+        filenames = self.get_filenames(generator_name, data_dir)
 
-            while len(filenames) < len_pdg_types:
-                wildcard_pattern = self.get_wildcard_pattern(generator_name)
-                filenames = glob(self.data_dir + wildcard_pattern)
+        # Merged GiBUU files are in a different directory
+        if generator_name == 'GiBUU':
+            data_dir = self.gibuu_dir
+
+        # Debug filename matching
+        assert len(
+            filenames
+        ) != 0, f"Matched files are zero. Trying to retrive files {filenames}"
+
+        # The dataset has to include all pdg types (should not be a big issue for GiBUU)
+        len_pdg_types = 4
+        while len(filenames) % len_pdg_types == 0:
+            filenames = self.get_filenames(generator_name, data_dir)
 
         data, weights = self._rootfile_to_array(filenames[0])
 
@@ -77,14 +141,26 @@ class ROOTDataset(Dataset):
             new_data, new_weights = self._rootfile_to_array(filename)
             data = np.append(data, new_data, axis=0)
             weights = np.append(weights, new_weights, axis=0)
-    
+            if generator_name == 'GiBUU':
+                self.weights_sum_b = np.sum(weights)
+            else:
+                self.weights_sum_a = np.sum(weights)
+
+        if generator_name == 'GiBUU':
+            weights = self.normalize_weights(weights, self.weights_sum_a,
+                                             self.weights_sum_b)
         # Create Labels
         labels = self.create_labels(data, filename)
         return np.hstack([data, labels, weights])
 
+    def normalize_weights(self, weights, weights_sum_a, weights_sum_b):
+        assert weights_sum_a != 0 or weights_sum_b != 0, 'Weight sums are zero'
+        ratio = weights_sum_b / weights_sum_a
+        print(f'Weight ratio is {ratio} diving by this to normalize weights')
+        return weights / ratio
 
     def create_labels(self, data, filename):
-        if 'GENIEv2' in filename:
+        if self.generator_a in filename:
             labels = np.zeros(len(data))
         else:
             labels = np.ones(len(data))
@@ -99,30 +175,31 @@ class ROOTDataset(Dataset):
                                     size=number_of_indices)
         return indices
 
-    def get_wildcard_pattern(self, generator_name):
+    def get_filenames(self, generator_name, data_dir, max_file_index=50):
         # String formatting to get the filename in the correct way
         if generator_name == 'GiBUU':
-            number_of_indices = 10
-            max_file_index = 150
-            if self.test_data:
-                indices = np.arange(start=100, stop=200, step=1)
-            else:
-                indices = self.draw_file_indices(max_file_index,
-                                                 number_of_indices)
-            filename_patterns = [
-                self.format_filename(generator_name, index)
-                for index in indices
-            ]
-            return filename_patterns
+            max_file_index = 4
 
-        indices = self.draw_file_indices()
-        return self.format_filename(generator_name, indices[0])
+        indices = self.draw_file_indices(max_file_index,
+                                         self.number_of_indices)
 
-    def format_filename(self, generator_name, indices):
+        # filenames = []
+        # for index in indices:
+
+        #     filenames.append()
+        filenames = [
+            glob(data_dir + self.format_filename(generator_name, index))
+            for index in indices
+        ]
+        flatttened_filenames = list(itertools.chain(*filenames))
+
+        return flatttened_filenames
+
+    def format_filename(self, generator_name, index):
         """
         Creates a ROOT filename wildcard with the passed index
         """
-        formatted_index = str(indices).zfill(3)
+        formatted_index = str(index).zfill(3)
         filename_pattern = f"*{generator_name}*_1M_{formatted_index}_NUISFLAT.root"
         return filename_pattern
 
